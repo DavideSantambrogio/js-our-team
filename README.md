397c5ba29e3a12ee2773ab374ea56009434afab4
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-11-06 16:57:23 +0100

    add: bonus

---
+++ README.md
@@ -0,0 +1,15 @@
+### Esercizio
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+### Svolgimento
+- [X] Creare l’array di oggetti con le informazioni fornite.
+- [X] Stampare su console le informazioni di nome, ruolo e la stringa della foto
+- [X] Stampare le stesse informazioni su DOM sottoforma di stringhe
+- [X] Trasformare la stringa foto in una immagine effettiva
+- [X] Organizzare i singoli membri in card/schede. 
+
+
+
+
+<img src="img/angela-caroll-chief-editor.jpg" alt="">
---
+++ js/script.js
@@ -0,0 +1,64 @@
+const cardContainer= document.getElementById("my-card")
+
+const team = [
+  {
+    "nome": "Wayne Barnett",
+    "ruolo": "Founder & CEO",
+    "foto": "wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    "nome": "Angela Caroll",
+    "ruolo": "Chief Editor",
+    "foto": "angela-caroll-chief-editor.jpg"
+  },
+  {
+    "nome": "Walter Gordon",
+    "ruolo": "Office Manager",
+    "foto": "walter-gordon-office-manager.jpg"
+  },
+  {
+    "nome": "Angela Lopez",
+    "ruolo": "Social Media Manager",
+    "foto": "angela-lopez-social-media-manager.jpg"
+  },
+  {
+    "nome": "Scott Estrada",
+    "ruolo": "Developer",
+    "foto": "scott-estrada-developer.jpg"
+  },
+  {
+    "nome": "Barbara Ramos",
+    "ruolo": "Graphic Designer",
+    "foto": "barbara-ramos-graphic-designer.jpg"
+  }
+]
+// console.log(team)
+
+
+// stampa di ogni array
+for (const member in team) {
+    console.log(team[member]);
+}
+
+
+// crea card
+for (let i = 0; i < team.length; i++) {
+
+    const newCard = document.createElement("div");    
+    newCard.classList.add("col-lg-4") 
+    newCard.classList.add("col-md-6")
+    newCard.classList.add("col-sm-12")
+    newCard.classList.add("mb-5")
+
+    newCard.innerHTML = (
+        '<img src="img/' + team[i].foto + '" alt="";>'
+        + '<h2>' + team[i].nome + '</h2>'
+        + '<p>' + team[i].ruolo + '</p>'
+    )
+        cardContainer.append(newCard)
+}
+
+
+{/* <img src="img/angela-caroll-chief-editor.jpg" alt=""> */}
+
+
